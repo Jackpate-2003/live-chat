9960f865362813c8e6c7337d643f616ece54f135
Jackpate-2003 <jackpate20032003@gmail.com>
2021-10-11 16:42:00 +0330

    First commit!

--- server.js
+++ server.js
@@ -1,27 +1,45 @@
 const express = require("express");
-const app = express();
-const server = require("http").Server(app);
+const next = require('next');
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
 const { v4: uuidv4 } = require("uuid");
-app.set("view engine", "ejs");
-const io = require("socket.io")(server, {
-  cors: {
-    origin: '*'
-  }
-});
+
+app.prepare().then(() => {
+
+    const server = express();
+
+    server.set("view engine", "ejs");
+
+
 const { ExpressPeerServer } = require("peer");
 const peerServer = ExpressPeerServer(server, {
   debug: true,
 });
 
-app.use("/peerjs", peerServer);
-app.use(express.static("public"));
+server.use("/peerjs", peerServer);
+server.use(express.static("public"));
 
-app.get("/", (req, res) => {
+server.get("/", (req, res) => {
   res.redirect(`/${uuidv4()}`);
 });
 
-app.get("/:room", (req, res) => {
+server.get("/:room", (req, res) => {
   res.render("room", { roomId: req.params.room });
+});
+
+server.all('*', (req, res) => {
+        return handle(req, res);
+    })
+
+const run = server.listen('3030', (err) => {
+        if (err) throw err;
+        console.log(`> Ready on http://localhost:${'3030'}`);
+    });
+const io = require("socket.io")(run, {
+  cors: {
+    origin: '*'
+  }
 });
 
 io.on("connection", (socket) => {
@@ -34,4 +52,4 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(process.env.PORT || 3030);
+});
