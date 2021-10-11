816a62fc83ee1cdb09535cfc6e7314aea325c314
Jackpate-2003 <jackpate20032003@gmail.com>
2021-10-11 17:43:41 +0330

    aa

--- server.js
+++ server.js
@@ -1,11 +1,43 @@
 const express = require("express");
 const next = require('next');
 const dev = process.env.NODE_ENV !== 'production';
-const app = next({ dev });
-const handle = app.getRequestHandler();
 const { v4: uuidv4 } = require("uuid");
 
-app.prepare().then(() => {
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    return res.redirect(`/${uuidv4()}`);
+});
+
+app.get("/:room", (req, res) => {
+    return res.render("room", { roomId: req.params.room });
+});
+
+const server = app.listen('3030', () => {
+    console.log("Listening on port: " + '3030');
+});
+
+const io = require("socket.io")(server, {
+    cors: {
+        origin: '*'
+    }
+});
+
+io.on("connection", (socket) => {
+    socket.on("join-room", (roomId, userId, userName) => {
+        socket.join(roomId);
+        socket.to(roomId).broadcast.emit("user-connected", userId);
+        socket.on("message", (message) => {
+            io.to(roomId).emit("createMessage", message, userName);
+        });
+    });
+});
+
+/*app.prepare().then(() => {
 
     const server = express();
 
@@ -21,35 +53,17 @@ server.use("/peerjs", peerServer);
 server.use(express.static("public"));
 
 server.get("/", (req, res) => {
-  res.redirect(`/${uuidv4()}`);
+  return res.redirect(`/${uuidv4()}`);
 });
 
 server.get("/:room", (req, res) => {
-  res.render("room", { roomId: req.params.room });
+  return res.render("room", { roomId: req.params.room });
 });
-
-server.all('*', (req, res) => {
-        return handle(req, res);
-    })
 
 const run = server.listen('3030', (err) => {
         if (err) throw err;
         console.log(`> Ready on http://localhost:${'3030'}`);
     });
-const io = require("socket.io")(run, {
-  cors: {
-    origin: '*'
-  }
-});
 
-io.on("connection", (socket) => {
-  socket.on("join-room", (roomId, userId, userName) => {
-    socket.join(roomId);
-    socket.to(roomId).broadcast.emit("user-connected", userId);
-    socket.on("message", (message) => {
-      io.to(roomId).emit("createMessage", message, userName);
-    });
-  });
-});
 
-});
+});*/
